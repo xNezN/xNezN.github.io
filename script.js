46253a62dbef2327b9ed0262da9d67934d73d852
xNezN <xnez.polonez@gmail.com>
2024-07-10 16:25:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,36 @@
 function getRandomPokemonId() {
-    return Math.floor(Math.random() * 1010) + 1;
-}
+            return Math.floor(Math.random() * 1010) + 1;
+        }
 
 async function fetchPokemonData() {
     const pokemonId = getRandomPokemonId();
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
     if (!response.ok) {
-        console.error('Failed to fetch Pokémon data:', response.statusText);
+        console.error("Failed to fetch Pokemon data:", +response.statusText)
         return;
     }
     const pokemon = await response.json();
     displayPokemon(pokemon);
 }
 
-const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-
-const capitalizePokemonName = (name) => {
-    if (name.includes('-')) {
-        return name
-            .split('-')
-            .map(word => capitalize(word))
-            .join(' ');
-    } else if (name.split(' ').length > 1) {
-        return name
-            .split(' ')
-            .map(word => capitalize(word))
-            .join(' ');
-    } else {
-        return capitalize(name);
-    }
-};
-
 function displayPokemon(pokemon) {
     const pokemonContainer = document.getElementById('pokemon-container');
     const pokemonName = document.createElement('h1');
     const pokemonImage = document.createElement('img');
-
-    const capitalizedName = capitalizePokemonName(pokemon.name);
-
+    
+    const capitalizedName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    
     pokemonName.textContent = capitalizedName;
     pokemonName.classList.add('pokemon-name');
-
+    
     pokemonImage.src = pokemon.sprites.front_default;
     pokemonImage.alt = pokemon.name;
     pokemonImage.classList.add('pokemon-image');
-
-     pokemonContainer.innerHTML = '';
-     pokemonContainer.appendChild(pokemonName);
-     pokemonContainer.appendChild(pokemonImage);
+    
+    pokemonContainer.innerHTML = '';
+    
+    pokemonContainer.appendChild(pokemonName);
+    pokemonContainer.appendChild(pokemonImage);
+}
 
 document.addEventListener('DOMContentLoaded', fetchPokemonData);
